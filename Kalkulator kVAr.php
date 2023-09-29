965c8b96222489fc3b8dc2154e7649bf9ce6eae3
dash rope <37311677+dasrop@users.noreply.github.com>
2023-09-29 22:42:00 +0700

    Update Kalkulator kVAr.php
    mengatasi kesalahan "Uncaught TypeError: $ is not a function",  perubahan yang dilakukan adalah menggunakan jQuery(document).ready(function($) { ... }) untuk menghindari konflik dengan perpustakaan JavaScript lain yang mungkin menggunakan $. Selain itu, kelas-kelas CSS dan efek-efeknya telah diterapkan pada elemen-elemen HTML sesuai dengan kelas yang telah Anda definisikan.

--- Kalkulator kVAr.php
+++ Kalkulator kVAr.php
@@ -4,33 +4,48 @@
   <title>Kalkulator kVAr Kapasitor</title>
   <style>
     /* CSS untuk tombol */
-    .orange-button {
-  background-color: orange;
-  border: 2px solid orange;
-  border-radius: 4px;
-  color: white;
-  padding: 10px 20px;
-  text-align: center;
-  text-decoration: none;
-  display: inline-block;
-  font-size: 16px;
-  cursor: pointer;
-  transition: background-color 0.3s ease;
-}
+    .custom-button {
+      background-color: orange;
+      border: 2px solid orange;
+      border-radius: 4px;
+      color: white;
+      padding: 10px 20px;
+      text-align: center;
+      text-decoration: none;
+      display: inline-block;
+      font-size: 16px;
+      cursor: pointer;
+      transition: background-color 0.3s ease, transform 0.3s ease;
+    }
 
     /* CSS untuk efek hover pada tombol */
-    .orange-button:hover {
+    .custom-button:hover {
       background-color: darkorange;
+      transform: scale(1.05); /* Efek perbesar saat dihover */
+    }
+
+    /* CSS untuk input form */
+    .custom-input {
+      padding: 10px;
+      border: 1px solid #ccc;
+      border-radius: 4px;
+      transition: border-color 0.3s ease, box-shadow 0.3s ease;
+    }
+
+    /* CSS untuk efek hover pada input form */
+    .custom-input:hover {
+      border-color: orange;
+      box-shadow: 0 0 5px orange; /* Efek bayangan saat dihover */
     }
 
     /* CSS untuk sel hasil */
-    .result {
+    .custom-result {
       font-weight: bold;
       padding-top: 10px;
     }
 
     /* CSS untuk label peringatan */
-    .warning {
+    .custom-warning {
       color: red;
       font-size: 12px;
       padding-top: 5px;
@@ -45,36 +60,36 @@
     <tbody>
       <tr>
         <td>Masukkan faktor daya saat ini (COS Φ1):</td>
-        <td><input id="cpf" type="text" /></td>
+        <td><input id="cpf" class="custom-input" type="text" /></td>
       </tr>
       <tr>
         <td>Masukkan beban saat ini (kW):</td>
-        <td><input id="cl" type="text" /></td>
+        <td><input id="cl" class="custom-input" type="text" /></td>
       </tr>
       <tr>
         <td>Masukkan Faktor Daya yang diinginkan (COS Φ2):</td>
-        <td><input id="rpf" type="text" /></td>
+        <td><input id="rpf" class="custom-input" type="text" /></td>
       </tr>
       <tr>
         <td><b>Kapasitor kVAr yang Dibutuhkan:</b></td>
-        <td class="result"></td>
+        <td class="custom-result"></td>
       </tr>
       <tr>
-        <td colspan="2" class="warning" id="warning-label"></td>
+        <td colspan="2" class="custom-warning" id="warning-label"></td>
       </tr>
       <tr>
         <td style="text-align: right;">
-          <input id="calckvar" class="orange-button" type="button" value="Hitung" />
+          <input id="calckvar" class="custom-button" type="button" value="Hitung" />
         </td>
         <td style="text-align: left;">
-          <input id="reset" class="orange-button" type="button" value="Reset" />
+          <input id="reset" class="custom-button" type="button" value="Reset" />
         </td>
       </tr>
     </tbody>
   </table>
 
   <script>
-    $(document).ready(function() {
+    jQuery(document).ready(function($) {
       $('input#calckvar').click(function() {
         var a = $('input#cpf').val();
         var b = $('input#cl').val();
@@ -89,14 +104,14 @@
         } else {
           $('#warning-label').text("");
           var answer = Math.round(parseFloat(b) * (Math.tan(Math.acos(parseFloat(a))) - Math.tan(Math.acos(parseFloat(c)))));
-          $('table.calc td.result').text(answer);
+          $('table.calc td.custom-result').text(answer);
         }
       });
 
       $('input#reset').click(function() {
         $('input[type="text"]').val("");
         $('#warning-label').text("");
-        $('table.calc td.result').text("");
+        $('table.calc td.custom-result').text("");
       });
 
       function isNumeric(value) {
